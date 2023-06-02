d91c44efc81fe3093618ef3ccfd2e3731992ad9c
henryhuangbc <85907185+henryhuangbc@users.noreply.github.com>
2023-06-01 19:11:10 -0700

    Update db.js

--- root/backend/config/db.js
+++ root/backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const defualt = require('default');
+const db = default.mongoURI; 
 
 const connectDB = async () => {
     try {
